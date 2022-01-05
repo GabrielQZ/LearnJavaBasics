d8856c565d38c16c3cb5ee1a559423110e0bd9b2
GabrielQZ <gabrielquinn.photography@gmail.com>
2022-01-04 19:15:45 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  
+        String firstName = "Gabriel";
+        String lastName = "Sherman";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+    }
+}
